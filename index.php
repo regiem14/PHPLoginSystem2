a48b7b00d84089f578bbdd4a0e12c727313c1967
Regie Marzan <lonebeginner@gmail.com>
2023-05-18 11:49:31 +0800

    add section, script w cdn react & adjust height 100% w css

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FIRST PHP LOGIN SYSTEM</title>
     <link rel="stylesheet" href="css/styles.css">
+    <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
+    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
 </head>
 <body>
     <header>
@@ -15,5 +18,26 @@
         ?>
         </div>
     </header>
+    <section>
+        <h2>There is another PHP Login System</h2>
+        <ul>
+            <li>You can see it</li>
+            <li>You can copy</li>
+            <li>You can review</li>
+            <li>You can see videos</li>
+        </ul>
+    </section>
+    <div id="mydiv"></div>
+
+    <script type="text/babel">
+      function Hello() {
+        return <h1>Hello World!</h1>;
+      }
+
+      const container = document.getElementById('mydiv');
+      const root = ReactDOM.createRoot(container);
+      root.render(<Hello />)
+    </script>
+
 </body>
 </html>
