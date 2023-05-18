76aa035ec4cbf744a04529809e9ab7ef9aaecfd7
Regie Marzan <lonebeginner@gmail.com>
2023-05-18 11:18:36 +0800

    add header, index.php & css

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FIRST PHP LOGIN SYSTEM</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <div class="header">
+        <?php
+        echo "Regie is Here for PHP script";
+        ?>
+        </div>
+    </header>
+</body>
+</html>
