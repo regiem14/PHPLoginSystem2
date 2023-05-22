17e8ef1eb248c1e3a5f9b403c86c5a5096b9680f
Regie Marzan <lonebeginner@gmail.com>
2023-05-22 20:02:39 +0800

    Built in Functions or Internal Functions to the languages

--- functions.php
+++ functions.php
@@ -6,7 +6,11 @@ include_once 'includes/header.php';
     <section>
         <div class="functions">
             <h2>Functions</h2>
-            <h3>Two Notes</h3>            
+            <h3>Two Notes</h3>
+            
+            <?php
+            echo "<h2>Built in Functions or Internal Functions to the languages</h2>";
+            ?>
         </div>
     </section>
 </main>
