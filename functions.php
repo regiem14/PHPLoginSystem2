d2c86de919f305af3e75beae046bf3814324e6b5
Regie Marzan <lonebeginner@gmail.com>
2023-05-22 19:38:16 +0800

    functions, add notes

--- functions.php
+++ functions.php
@@ -5,7 +5,8 @@ include_once 'includes/header.php';
 <main>
     <section>
         <div class="functions">
-            
+            <h2>Functions</h2>
+            <h3>Two Notes</h3>            
         </div>
     </section>
 </main>
