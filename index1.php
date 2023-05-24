ae18c646c4ff201f2342637a364b313761659ac6
Regie Marzan <lonebeginner@gmail.com>
2023-05-24 20:34:37 +0800

    add example.txt & class Fruit in index1

--- index1.php
+++ index1.php
@@ -18,5 +18,28 @@
     <h2>MVC Model</h2>
 
     <h3>Tasks of PHP</h3>
+    <?php 
+            class Fruit{
+                public $name;
+                public $color;
+
+                function setName($name){
+                    $this->name= $name;
+                }
+
+                function getName(){
+                    return $this->name;
+                }
+            }
+
+        $apple = new Fruit();
+        $banana = new Fruit();
+        $apple->setName('Apple');
+        $banana->setName('Banana');
+        
+        echo $apple->getName();
+        echo "<br>";
+        echo $banana->getName();
+        ?>
 </body>
 </html>
