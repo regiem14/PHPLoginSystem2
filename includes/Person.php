3bc080d2617a2a689d4096e3a751cd8916cf1eda
Regie Marzan <lonebeginner@gmail.com>
2023-05-25 21:08:22 +0800

    add construct & destruct example

--- includes/Person.php
+++ includes/Person.php
@@ -15,16 +15,23 @@ class Person{
         $this->eyeColor = $newEyeColor;
         $this->age = $newAge;
     }
-
-    public function getName(){
-        return $this->name;
-    }
-
-    public function getEyeColor(){
-        return $this->eyeColor;
-    }
     
-    public function getAge(){
-        return $this->age;
+    public function __destruct()
+    {
+        echo "{$this->name} is here." . "<br>";
+        echo "My eye color is{$this->eyeColor}." . "<br>";
+        echo "I'm {$this->age} years old." . "<br>";
     }
+
+    // public function getName(){
+    //     return $this->name;
+    // }
+
+    // public function getEyeColor(){
+    //     return $this->eyeColor;
+    // }
+    
+    // public function getAge(){
+    //     return $this->age;
+    // }
 }
