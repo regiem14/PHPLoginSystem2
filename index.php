153600c148b5dcd759c4414dab84679986b4c2ad
Regie Marzan <lonebeginner@gmail.com>
2023-05-26 19:48:58 +0800

    add includes/header

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OOP2</title>
-</head>
+<?php
+include_once 'includes/header.php';
+?>
 <body>
     <h1>OOP - Object Oreinted Programming</h1>
 
